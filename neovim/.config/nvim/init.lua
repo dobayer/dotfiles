4531cd789d3b84e9d1b58d825fa44489d4548fee
Dominik Bayer <dominik.bayer@datev.de>
2024-06-12 13:34:26 +0200

    nvim noice, twilight, precognition

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -2,9 +2,6 @@
 -- See `:help mapleader`
 -- NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
 
-
-
-
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
@@ -18,22 +15,22 @@ require "keymaps"
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  vim.fn.system { "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath }
+    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+    vim.fn.system { "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath }
 end
 
 ---@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-  -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
-  "tpope/vim-sleuth", -- Detect tabstop and shiftwidth automatically
+    -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
+    "tpope/vim-sleuth", -- Detect tabstop and shiftwidth automatically
 
-  import = "plugins",
+    import = "plugins"
 }, {
-  change_detection = {
-    notify = false,
-  },
+    change_detection = {
+        notify = false,
+    },
 })
 
 -- The line beneath this is called `modeline`. See `:help modeline`
