75cc34dc037efbcee4ec66b14fd086afad1883a8
Dominik Bayer <dominik.bayer@datev.de>
2024-06-17 09:47:26 +0200

    nvim precognition off. mini modules

--- neovim/.config/nvim/lua/plugins/mini.lua
+++ neovim/.config/nvim/lua/plugins/mini.lua
@@ -1,10 +1,16 @@
 return {
-  {
-    "echasnovski/mini.nvim",
-    config = function()
-      require("mini.ai").setup()
-      require("mini.surround").setup()
-      --require("mini.statusline").setup()
-    end,
-  },
+    {
+        "echasnovski/mini.nvim",
+        config = function()
+            require("mini.ai").setup()
+            require("mini.surround").setup()
+            require("mini.pairs").setup()
+            require("mini.extra").setup()
+            require("mini.animate").setup()
+            require("mini.basics").setup()
+            require("mini.clue").setup()
+
+            --require("mini.statusline").setup()
+        end,
+    },
 }
